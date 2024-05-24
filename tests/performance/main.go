ea29df9c790ea4a456312ad53629d9eed8663ddc
Heathcliff <heathcliff@heathcliff.eu>
2024-05-24 16:24:08 +0200

    Add config flags to performance test
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- tests/performance/main.go
+++ tests/performance/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
@@ -10,21 +11,45 @@ import (
 	"github.com/heathcliff26/go-minesweeper/pkg/minesweeper"
 )
 
-const workerThreads = 10
+var (
+	workerThreads            int
+	iterationsPerMeasureLoop int
+	printMeasurements        bool
+)
+
+var usage bool
+
+const measurementsPerLine = 10
+
+func init() {
+	flag.IntVar(&iterationsPerMeasureLoop, "i", 10000, "Iterations per measurement")
+	flag.IntVar(&workerThreads, "w", 10, "Simoultaneous worker threads")
+	flag.BoolVar(&printMeasurements, "v", false, "Print measurements in addition to average")
+	flag.BoolVar(&usage, "h", false, "Show helptext")
+}
 
 func main() {
+	flag.Parse()
+	if usage {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	fmt.Printf("Settings: threads=%d, iterations=%d\n\n", workerThreads, iterationsPerMeasureLoop)
+
 	difficulty, _ := minesweeper.NewCustomDifficulty(700, 99, 99)
-	measureLoop("NewGameWithSafePos", 100, func(res chan time.Duration) {
+	measureLoop("NewGameWithSafePos", iterationsPerMeasureLoop, func(res chan time.Duration) {
 		measure(res, func() {
 			minesweeper.NewGameWithSafePos(difficulty, minesweeper.NewPos(50, 50))
 		})
 	})
-	measureLoop("NewGameWithSafeArea", 100, func(res chan time.Duration) {
+	measureLoop("NewGameWithSafeArea", iterationsPerMeasureLoop, func(res chan time.Duration) {
 		measure(res, func() {
 			minesweeper.NewGameWithSafeArea(difficulty, minesweeper.NewPos(50, 50))
 		})
 	})
-	measureLoop("AssistedMode", 100, measureAssistedMode)
+
+	measureLoop("AssistedMode", iterationsPerMeasureLoop, measureAssistedMode)
 }
 
 // Run the measurement multiple times, running in parallel as set by workerThreads
@@ -56,17 +81,20 @@ func measureLoop(name string, iterations int, f func(res chan time.Duration)) st
 
 	fmt.Printf("Finished measuring %s, calculating result\n\n", name)
 	max, min, average := calculateStats(measurements)
-	output := fmt.Sprintf("Name: %s\nMax: %s\nMin: %s\nAverage: %s\nMeasurements:\n", name, dString(max), dString(min), dString(average))
-	for i := 0; i < iterations; i++ {
-		output += dString(measurements[i])
-		if (i+1)%workerThreads == 0 {
-			output += "\n"
-		} else {
-			output += " "
+	output := fmt.Sprintf("Name: %s\nMax: %s\nMin: %s\nAverage: %s\n", name, dString(max), dString(min), dString(average))
+	if printMeasurements {
+		fmt.Println("Measurements:")
+		for i := 0; i < iterations; i++ {
+			output += dString(measurements[i])
+			if (i+1)%measurementsPerLine == 0 {
+				output += "\n"
+			} else {
+				output += " "
+			}
 		}
-	}
-	if !(iterations%workerThreads == 0) {
-		output += "\n"
+		if !(iterations%measurementsPerLine == 0) {
+			output += "\n"
+		}
 	}
 	fmt.Print(output + "\n")
 	return output
