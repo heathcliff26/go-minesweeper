6194af306e8d21140bdfc13c0481d1cfa456cbfe
Heathcliff <heathcliff@heathcliff.eu>
2024-05-26 09:52:59 +0200

    cli: Add flags for enabling more verbose output to log
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- cmd/app/log.go
+++ cmd/app/log.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log/slog"
 	"os"
 )
 
-const logLevel = slog.LevelError
+var (
+	logDebug bool
+	logInfo  bool
+)
 
 func init() {
+	flag.BoolVar(&logDebug, "vv", false, "Enable more verbose logging")
+	flag.BoolVar(&logInfo, "v", false, "Enable more verbose logging")
+}
+
+func initializeLogger() {
+	var logLevel slog.Level
+	switch {
+	case logDebug:
+		logLevel = slog.LevelDebug
+	case logInfo:
+		logLevel = slog.LevelInfo
+	default:
+		logLevel = slog.LevelError
+	}
+
 	opts := slog.HandlerOptions{
 		Level: logLevel,
 	}
