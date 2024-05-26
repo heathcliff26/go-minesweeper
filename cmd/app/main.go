142f21f779c0dd280acef169c32c342703e22eca
Heathcliff <heathcliff@heathcliff.eu>
2024-05-26 09:52:59 +0200

    cli: Add flags for enabling more verbose output to log
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "github.com/heathcliff26/go-minesweeper/pkg/app"
+import (
+	"flag"
+
+	"github.com/heathcliff26/go-minesweeper/pkg/app"
+)
 
 func main() {
+	flag.Parse()
+	initializeLogger()
+
 	app := app.New()
 	app.Run()
 }
