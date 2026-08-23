6bdef64e4a71b43e234e947673fd6a04afa6c225
Heathcliff <heathcliff@heathcliff.eu>
2026-08-23 12:54:45 +0200

    unit-test: Use synctest and reduce time usage
    Ensure faster, more predictable test output by using synctest.
    Reduce usage of time.Sleep through using channels where possible.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- pkg/app/timer_test.go
+++ pkg/app/timer_test.go
@@ -2,15 +2,14 @@ package app
 
 import (
 	"testing"
+	"testing/synctest"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTimer(t *testing.T) {
-	t.Parallel()
 	t.Run("New", func(t *testing.T) {
-		t.Parallel()
 		assert := assert.New(t)
 
 		timer := NewTimer()
@@ -19,51 +18,54 @@ func TestTimer(t *testing.T) {
 		assert.Equal("0000", timer.Label.Text)
 	})
 	t.Run("Counting", func(t *testing.T) {
-		t.Parallel()
-		assert := assert.New(t)
+		synctest.Test(t, func(t *testing.T) {
+			assert := assert.New(t)
 
-		timer := NewTimer()
+			timer := NewTimer()
 
-		timer.Start()
-		assert.True(timer.running)
+			timer.Start()
+			assert.True(timer.running)
 
-		time.Sleep(10*time.Second + 500*time.Millisecond)
-		timer.Stop()
+			synctest.Sleep(10*time.Second + 500*time.Millisecond)
+			timer.Stop()
 
-		assert.False(timer.running)
-		assert.Equal(10, timer.Seconds)
-		assert.Equal("0010", timer.Label.Text)
+			assert.False(timer.running)
+			assert.Equal(10, timer.Seconds)
+			assert.Equal("0010", timer.Label.Text)
+		})
 	})
 	t.Run("Reset", func(t *testing.T) {
-		t.Parallel()
-		assert := assert.New(t)
+		synctest.Test(t, func(t *testing.T) {
+			assert := assert.New(t)
 
-		timer := NewTimer()
+			timer := NewTimer()
 
-		timer.Start()
-		assert.True(timer.running)
+			timer.Start()
+			assert.True(timer.running)
 
-		time.Sleep(2 * time.Second)
+			synctest.Sleep(2 * time.Second)
 
-		timer.Reset()
-		assert.False(timer.running)
-		assert.Equal(0, timer.Seconds)
-		assert.Equal("0000", timer.Label.Text)
+			timer.Reset()
+			assert.False(timer.running)
+			assert.Equal(0, timer.Seconds)
+			assert.Equal("0000", timer.Label.Text)
+		})
 	})
 	t.Run("MultipleCallsToStart", func(t *testing.T) {
-		t.Parallel()
-		assert := assert.New(t)
+		synctest.Test(t, func(t *testing.T) {
+			assert := assert.New(t)
 
-		timer := NewTimer()
+			timer := NewTimer()
 
-		timer.Start()
-		assert.True(timer.running)
-		timer.Start()
-		assert.True(timer.running)
+			timer.Start()
+			assert.True(timer.running)
+			timer.Start()
+			assert.True(timer.running)
 
-		time.Sleep(10*time.Second + 500*time.Millisecond)
-		timer.Stop()
+			synctest.Sleep(10*time.Second + 500*time.Millisecond)
+			timer.Stop()
 
-		assert.Equal(10, timer.Seconds)
+			assert.Equal(10, timer.Seconds)
+		})
 	})
 }
